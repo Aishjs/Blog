3e2c8561ba28e4f8c32e2d49c0e5fbc2ad7ac147
Aiswarya <Aish@Vysakhs-MacBook-Air.local>
2023-10-04 19:58:37 +0100

    changes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const socket = require("socket.io");
+const path = require("path");
+
 
 // App setup
 const PORT = 5000;
@@ -15,26 +17,16 @@ app.use(express.static("public"));
 // Socket setup
 const io = socket(server);
 
-//we use a set to store users, sets objects are for unique values of any type
-const activeUsers = new Set();
-
 io.on("connection", function (socket) {
   console.log("Made socket connection");
 
-  socket.on("new user", function (data) {
-    socket.userId = data;
-    activeUsers.add(data);
-    //... is the the spread operator, adds to the set while retaining what was in there already
-    io.emit("new user", [...activeUsers]);
+  socket.on("newuser", function (username) {
+    socket.broadcast.emit("update",username+"joined the conversation");
   });
-
-  socket.on("disconnect", function () {
-      activeUsers.delete(socket.userId);
-      io.emit("user disconnected", socket.userId);
-    });
-
-    socket.on("chat message", function (data) {
-      io.emit("chat message", data);
+  socket.on("exituser", function (username) {
+    socket.broadcast.emit("update",username+"left the conversation");
   });
-
+  socket.on("chat", function (message) {
+    socket.broadcast.emit("chat",message);
+  });
 });
--- public/js/client.js
+++ public/js/client.js
@@ -1,110 +1,79 @@
 //required for front end communication between client and server
 
-const socket = io();
+(function () {
+  const app = document.querySelector(".app");
+  const socket = io();
 
-const inboxPeople = document.querySelector(".inbox__people");
+  let userName = "";
 
-
-let userName = "";
-let id;
-const newUserConnected = function (data) {
-    
-
-    //give the user a random unique id
-    id = Math.floor(Math.random() * 1000000);
-    userName = 'user-' +id;
-    //console.log(typeof(userName));   
-    
-
-    //emit an event with the user id
-    socket.emit("new user", userName);
-    //call
-    addToUsersBox(userName);
-};
-
-const addToUsersBox = function (userName) {
-    //This if statement checks whether an element of the user-userlist
-    //exists and then inverts the result of the expression in the condition
-    //to true, while also casting from an object to boolean
-    if (!!document.querySelector(`.${userName}-userlist`)) {
-        return;
-    
+  app.querySelector("#join-user").addEventListener("click", function () {
+    let username = app.querySelector(".join-screen #username").value;
+    if (username.length == 0) {
+      return;
     }
-    
-    //setup the divs for displaying the connected users
-    //id is set to a string including the username
-    const userBox = `
-    <div class="chat_id ${userName}-userlist">
-      <h5>${userName}</h5>
-    </div>
-  `;
-    //set the inboxPeople div with the value of userbox
-    inboxPeople.innerHTML += userBox;
-};
-
-//call 
-newUserConnected();
-
-//when a new user event is detected
-socket.on("new user", function (data) {
-  data.map(function (user) {
-          return addToUsersBox(user);
-      });
-});
-
-//when a user leaves
-socket.on("user disconnected", function (userName) {
-  document.querySelector(`.${userName}-userlist`).remove();
-});
-
-
-const inputField = document.querySelector(".message_form__input");
-const messageForm = document.querySelector(".message_form");
-const messageBox = document.querySelector(".messages__history");
-
-const addNewMessage = ({ user, message }) => {
-  const time = new Date();
-  const formattedTime = time.toLocaleString("en-US", { hour: "numeric", minute: "numeric" });
-
-  const receivedMsg = `
-  <div class="incoming__message">
-    <div class="received__message">
-      <p>${message}</p>
-      <div class="message__info">
-        <span class="message__author">${user}</span>
-        <span class="time_date">${formattedTime}</span>
-      </div>
-    </div>
-  </div>`;
-
-  const myMsg = `
-  <div class="outgoing__message">
-    <div class="sent__message">
-      <p>${message}</p>
-      <div class="message__info">
-        <span class="time_date">${formattedTime}</span>
-      </div>
-    </div>
-  </div>`;
-
-  //is the message sent or received
-  messageBox.innerHTML += user === userName ? myMsg : receivedMsg;
-};
-
-messageForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  if (!inputField.value) {
-    return;
-  }
-
-  socket.emit("chat message", {
-    message: inputField.value,
-    nick: userName,
+    socket.emit("newuser", username);
+    userName = username;
+    app.querySelector(".join-screen").classList.remove("active");
+    app.querySelector(".chat-screen").classList.add("active");
   });
 
-  inputField.value = "";
-});
+  app.querySelector(".chat-screen #send-message").addEventListener("click", function () {
+    let message = app.querySelector(".chat-screen #message-input").value;
+    if (message.length == 0) {
+      return;
+    }
+    renderMessage("my", {
+      username: userName,
+      text: message,
+    });
+    socket.emit("chat", {
+      username: userName,
+      text: message,
+    });
+    app.querySelector(".chat-screen #message-input").value = "";
+  });
 
-socket.on("chat message", function (data) {
-  addNewMessage({ user: data.nick, message: data.message });
-});
+  app.querySelector(".chat-screen #exit-chat").addEventListener("click", function () {
+    socket.emit("exituser", userName);
+    window.location.href = window.location.href;
+  });
+
+  socket.on("update", function (update) {
+    renderMessage("update", update);
+  });
+
+  socket.on("chat", function (message) {
+    renderMessage("other", message);
+  });
+
+  function renderMessage(type, message) {
+    let messageContainer = app.querySelector(".chat-screen .messages");
+    if (type === "my") {
+      let ele = document.createElement("div");
+      ele.setAttribute("class", "message my-message");
+      ele.innerHTML = `
+        <div>
+          <div class="name">You</div>
+          <div class="text">${message.text}</div>
+        </div>
+      `;
+      messageContainer.appendChild(ele);
+    } else if (type === "other") {
+      let ele = document.createElement("div");
+      ele.setAttribute("class", "message other-message");
+      ele.innerHTML = `
+        <div>
+          <div class="name">${message.username}</div>
+          <div class="text">${message.text}</div>
+        </div>
+      `;
+      messageContainer.appendChild(ele);
+    } else if (type === "update") {
+      let ele = document.createElement("div");
+      ele.setAttribute("class", "update");
+      ele.innerText = message;
+      messageContainer.appendChild(ele);
+    }
+    messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
+  }
+})();
