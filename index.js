fad1488442e1eb6035d97de1ab5349f845164301
AISWARYA JAYASREE SASIDHARAN <a.jayasreesasidharan.22@abdn.ac.uk>
2023-10-06 10:36:34 +0000

    comments

--- index.js
+++ index.js
@@ -15,18 +15,17 @@ app.use(express.static("public"));
 // Socket setup
 const io = socket(server);
 
-// Initialize an array to store active users
-const activeUsers = [];
+//we use a set to store users, sets objects are for unique values of any type
+const activeUsers = new Set();
 
 io.on("connection", function (socket) {
   console.log("Made socket connection");
 
   socket.on("newuser", function (username) {
-    // Add the new user to the activeUsers array
-    activeUsers.push(username);
-    // Broadcast the updated active users list to all clients
-    socket.broadcast.emit("activeusers", activeUsers);
-
+    socket.userId = username;
+    activeUsers.add(username);
+    //... is the the spread operator, adds to the set while retaining what was in there already
+    io.emit("new user", [...activeUsers]);
     // Broadcast a message to inform others that the user has joined
     socket.broadcast.emit("update", username + " joined the conversation");
   });
@@ -48,43 +47,3 @@ io.on("connection", function (socket) {
     socket.broadcast.emit("chat", message);
   });
 });
-
-
-
-
-// const express = require("express");
-// const socket = require("socket.io");
-// const path = require("path");
-
-
-// // App setup
-// const PORT = 5000;
-// const app = express();
-// const server = app.listen(PORT, function () {
-//   console.log(`Listening on port ${PORT}`);
-
-// });
-
-// // Static files
-// app.use(express.static("public"));
-
-// // Socket setup
-// const io = socket(server);
-// const activeUsers = new Set();
-
-// io.on("connection", function (socket) {
-//   console.log("Made socket connection");
-
-//   socket.on("newuser", function (username) {
-//     socket.userName = username;
-//     activeUsers.add(username);
-//     socket.broadcast.emit("activeusers", activeUsers);
-//     socket.broadcast.emit("update" , username +"joined the conversation");
-//   });
-//   socket.on("exituser", function (username) {
-//     socket.broadcast.emit("update", username +"left the conversation");
-//   });
-//   socket.on("chat", function (message) {
-//     socket.broadcast.emit("chat",message);
-//   });
-// });
--- public/js/client.js
+++ public/js/client.js
@@ -2,25 +2,47 @@
 
 (function () {
   const app = document.querySelector(".app");
+  // create socket.io instance for client-server communication
   const socket = io();
+// variable to store the user's username
+  let userName = "";
+  //varibale to store the user's unique id
+  let id;
 
-  let userName = username;
+  // funtion when a new user joins
+  const newUserConnected = function (username) {
+    
 
+    //give the user a random unique id
+    id = Math.floor(Math.random() * 1000000);
+    userName = 'user-' +id;
+    //console.log(typeof(userName));   
+    
+
+    //emit an event with the user id
+    socket.emit("new user", userName);
+    //call
+    addToUsersBox(userName);
+};
+// Event-listner for join button click
   app.querySelector("#join-user").addEventListener("click", function () {
     let username = app.querySelector(".join-screen #username").value;
     if (username.length == 0) {
       return;
     }
+    // emit a new user event with the username entered
     socket.emit("newuser", username);
     userName = username;
     app.querySelector(".join-screen").classList.remove("active");
     app.querySelector(".chat-screen").classList.add("active");
   });
+  // event listener for the send button
   app.querySelector(".chat-screen #send-message").addEventListener("click", function () {
     let message = app.querySelector(".chat-screen #message-input").value;
     if (message.length == 0) {
       return;
     }
+    // render a chat message and emit a chat event to server
     renderMessage("my", {
       username: userName,
       text: message,
@@ -31,39 +53,45 @@
     });
     app.querySelector(".chat-screen #message-input").value = "";
   });
-
+// event listener for the exit button
   app.querySelector(".chat-screen #exit-chat").addEventListener("click", function () {
     socket.emit("exituser", userName);
     window.location.href = window.location.href;
   });
-
+// event listener for the update event received from the server
   socket.on("update", function (update) {
     renderMessage("update", update);
   });
-
+// event listener for the chat event received from the server
   socket.on("chat", function (message) {
     renderMessage("other", message);
   });
-
+// function to render chat messages
   function renderMessage(type, message) {
+    const time = new Date();
+    const formattedTime = time.toLocaleString("en-US", { hour: "numeric", minute: "numeric" });
     let messageContainer = app.querySelector(".chat-screen .messages");
     if (type === "my") {
+      // create and append message element for the user's message
       let ele = document.createElement("div");
       ele.setAttribute("class", "message my-message");
       ele.innerHTML = `
         <div>
           <div class="name">You</div>
           <div class="text">${message.text}</div>
+          <span class="time_date">${formattedTime}</span>
         </div>
       `;
       messageContainer.appendChild(ele);
     } else if (type === "other") {
+      // create and append message element for other user's message
       let ele = document.createElement("div");
       ele.setAttribute("class", "message other-message");
       ele.innerHTML = `
         <div>
           <div class="name">${message.username}</div>
           <div class="text">${message.text}</div>
+          <span class="time_date">${formattedTime}</span>
         </div>
       `;
       messageContainer.appendChild(ele);
@@ -73,6 +101,7 @@
       ele.innerText = message;
       messageContainer.appendChild(ele);
     }
+    // scroll the message to top
     messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
   }
 })();
